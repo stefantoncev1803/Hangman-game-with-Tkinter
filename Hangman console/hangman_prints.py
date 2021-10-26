b7cb92bc4a1825b60807ccdb437f3d8ba72ca5b7
stefantoncev1803 <93193314+stefantoncev1803@users.noreply.github.com>
2021-10-26 14:28:07 +0300

    Add files via upload

---
+++ Hangman console/hangman_prints.py
@@ -0,0 +1,47 @@
+def first_print():
+    print('''
+    ----''')
+    print()
+
+def second_print():
+    print(''' 
+    ----|
+        |
+        |''')
+    print()
+
+
+def third_print():
+    print(''' 
+    ----|
+        |
+        |
+        O
+           ''')
+    print()
+
+
+def fourth_print():
+    print(''' 
+    ----|
+        |
+        |
+        O
+        |
+        |
+            ''')
+    print()
+
+def fifth_print():
+    print('''
+    ----|
+        | 
+        |
+        |
+        O leelke
+       /|\ 
+        |
+       / \ 
+    == == == == == ''')
+    print()
+
